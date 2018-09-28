de21e4352bdfd52a3de3023d20ffd2c7b8ec9f91
kathyting <kathyfu85@163.com>
2018-09-28 12:02:13 -0700

    Delete Moody.java

--- Moody.java
+++
@@ -1,8 +0,0 @@
-
-public abstract class Moody {
-   protected abstract String getMood();
-   protected abstract void expressFeeling();
-   public void queryMood(){
-	   System.out.println("How are you feeling today?");
-   }
-}
